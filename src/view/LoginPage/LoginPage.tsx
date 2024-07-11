ef66ee00e4d51eb21b18dbd1f51dd36360e08da1
Andrey_Nesterok <and-nesterok@mail.ru>
2024-07-11 19:32:23 +0400

    refactoring

--- src/view/LoginPage/LoginPage.tsx
+++ src/view/LoginPage/LoginPage.tsx
@@ -3,16 +3,10 @@ import type { ILoginPageProps } from './types';
 import { Link } from 'react-router-dom';
 
 export const LoginPage = (props: ILoginPageProps): ReactNode => {
-	const arr: number[] = [1, 2, 3, 4, 5, 6];
-
 	return (
 		<div {...props}>
 			<h3>login page</h3>
-			{arr.map(item => (
-				<Link key={item} to={`/login/${item}`}>
-					way {item}
-				</Link>
-			))}
+			<Link to="/register" />
 		</div>
 	);
 };
--- src/view/RegisterPage/RegisterPage.tsx
+++ src/view/RegisterPage/RegisterPage.tsx
@@ -1,6 +1,12 @@
 import type { ReactNode } from 'react';
 import type { IRegsiterProps } from './types';
+import { Link } from 'react-router-dom';
 
 export const RegisterPage = (props: IRegsiterProps): ReactNode => {
-	return <div {...props}>RegisterPage</div>;
+	return (
+		<div {...props}>
+			<h3>RegisterPage</h3>
+			<Link to="/register" />
+		</div>
+	);
 };
