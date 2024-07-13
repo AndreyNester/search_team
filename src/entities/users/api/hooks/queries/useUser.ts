7b12b7eb42fe70295bb09245276024d648822185
Andrey_Nesterok <and-nesterok@mail.ru>
2024-07-13 15:19:22 +0400

    added header in profile page

--- src/entities/users/api/hooks/queries/useUser.ts
+++ src/entities/users/api/hooks/queries/useUser.ts
@@ -3,12 +3,15 @@ import { keepPreviousData, useQuery } from '@tanstack/react-query';
 import type { GetUserResponse, GetUserRequest } from '../../types';
 import { usersApi } from '../../usersApi';
 
-export interface IUseUserProps extends GetUserRequest {}
+export interface IUseUserProps extends GetUserRequest {
+	enabled?: boolean;
+}
 
-export const useUser = ({ id }: IUseUserProps): UseQueryResult<GetUserResponse> => {
+export const useUser = ({ id, enabled = true }: IUseUserProps): UseQueryResult<GetUserResponse> => {
 	return useQuery<GetUserResponse>({
 		queryKey: ['user', id],
 		queryFn: () => usersApi.getUser({ id }),
 		placeholderData: keepPreviousData,
+		enabled,
 	});
 };
--- src/view/ProfilePage/ProfilePage.tsx
+++ src/view/ProfilePage/ProfilePage.tsx
@@ -3,15 +3,64 @@ import type { IProfilePageProps } from './types';
 import { useParams } from 'react-router-dom';
 import { HeaderInfo } from '../../shared/header/ui/HeaderInfo/HeaderInfo';
 import { NotFoundPage } from '../NotFoundPage/NotFoundPage';
+import styles from './ProfilePage.module.css';
+import { useUser } from '../../entities/users/api/hooks/queries/useUser';
 
 export const ProfilePage = (props: IProfilePageProps): ReactNode => {
 	const { id } = useParams<{ id: string }>();
 	console.log(id);
 
+	const {
+		isPending: isPending_user,
+		isError: isError_user,
+		error: error_user,
+		data: data_user,
+		isFetching: isFetching_user,
+		isPlaceholderData: isPlaceholderData_user,
+	} = useUser({
+		id: Number(id),
+		/*
+		 Guard в случае ввода в поисковую строку не цифр
+		*/
+		enabled: !Number.isNaN(Number(id)),
+	});
+
 	return !Number.isNaN(Number(id)) ? (
 		<section {...props}>
-			<HeaderInfo>header info</HeaderInfo>
-			profile info
+			<HeaderInfo classNameOfContentWrapper={styles.headerContentWrapper}>
+				<div className={styles.CardsFieldContainer}>
+					{isFetching_user ? (
+						<div>loading...</div>
+					) : isError_user ? (
+						<div>something was wrong</div>
+					) : !data_user ? (
+						<div>no content on server</div>
+					) : (
+						<div className={styles.headerContainer}>
+							<div className={styles.headerTitle}>
+								<span className={styles.title}>
+									{data_user.data.first_name} {data_user.data.last_name}
+								</span>
+								<span className={styles.info}>Партнер</span>
+							</div>
+
+							<img src={data_user.data.avatar} alt="user icon" className={styles.image} />
+						</div>
+					)}
+				</div>
+			</HeaderInfo>
+			<div className={styles.CardsFieldContainer}>
+				{isFetching_user ? (
+					<div>loading...</div>
+				) : isError_user ? (
+					<div>something was wrong</div>
+				) : !data_user ? (
+					<div>no content on server</div>
+				) : (
+					// <CardsField data={data_user} />
+					<div>info</div>
+				)}
+			</div>
 		</section>
 	) : (
 		<NotFoundPage />
